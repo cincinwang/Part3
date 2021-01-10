eb825526dda1cfb0b777954152674413919b8b4b
cincinwang <cincinwang0510@gmail.com>
2021-01-10 11:00:50 +0200

    3.15-3.18

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors=require('cors');
-const Person = require('./models/person')
+const Person = require('./models/person');
 // const mongoose = require('mongoose');
 
 
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
+
+
 
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :POST"));
 morgan.token("POST", (request, response)=>JSON.stringify(request.body));
@@ -35,51 +37,55 @@ morgan.token("POST", (request, response)=>JSON.stringify(request.body));
 //     }
 // });
 
-
-let persons=[
-    {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1
-    },
-    {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-        id: 2
-    },
-    {
-        name: "Dan Abramov",
-        number: "12-43-234345",
-        id: 3
-    },
-    {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-        id: 4
-    },
-    {
-        name: "CinCin",
-        number: "39-00-6423122",
-        id: 5
-    }
-];
+//
+// let persons=[
+//     {
+//         name: "Arto Hellas",
+//         number: "040-123456",
+//         id: 1
+//     },
+//     {
+//         name: "Ada Lovelace",
+//         number: "39-44-5323523",
+//         id: 2
+//     },
+//     {
+//         name: "Dan Abramov",
+//         number: "12-43-234345",
+//         id: 3
+//     },
+//     {
+//         name: "Mary Poppendieck",
+//         number: "39-23-6423122",
+//         id: 4
+//     },
+//     {
+//         name: "CinCin",
+//         number: "39-00-6423122",
+//         id: 5
+//     }
+// ];
 
 // app.get('/', (request,response)=>{
 //     return response.send("it's working!")
 // });
 
-app.get('/api/persons', (request,response)=>{
+app.get('/api/persons', (request,response, next)=>{
     // response.json(persons)
     // console.log(response);
     Person.find({})
         .then(persons =>{
         response.json(persons)
-
     })
+        .catch(error=>next(error))
 });
 
-app.get('/info', (request,response)=>{
-    response.send(`Phonebook has info for ${persons.length} people <br> <P>${new Date()}</P>`)
+app.get('/info', (request,response, next)=>{
+    Person.find({}).then(persons=>{
+        response.send(`Phonebook has info for ${persons.length} people <br> <P>${new Date()}</P>`)
+    })
+        .catch(error=>next(error))
+
 });
 
 
@@ -98,10 +104,21 @@ app.get('/info', (request,response)=>{
 
 
 //refactor the backend to use the database:
-app.get('/api/persons/:id', (request,response)=>{
-    Person.findById(request.params.id).then(person=> {
-        response.json(person)
-    })
+//The next function is passed to the handler as the third parameter
+//If next was called without a parameter, then the execution would simply move onto the next route or middleware. If the next function is called with a parameter, then the execution will continue to the error handler middleware
+app.get('/api/persons/:id', (request,response, next)=>{
+    Person.findById(request.params.id)
+        .then(person=> {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error =>{next(error)
+            // console.log(error)
+            // response.status(400).send({error:'malformatted Id'})
+            })
 });
 
 
@@ -112,16 +129,17 @@ const generateId= ()=>{
 app.post('/api/persons',(request,response)=>{
     const body = request.body;
     // console.log(request);
+
      if(!body.name || !body.number){
          return response.status(400).json({
              error: 'name or number is missing'
          })
      }
-     if(persons.find(person=> person.name ===body.name)){
-         return response.status(400).json({
-             error:'name must be unique'
-         })
-     }
+     // if(persons.find(person=> person.name ===body.name)){
+     //     return response.status(400).json({
+     //         error:'name must be unique'
+     //     })
+     // }
 
 
      // const person ={
@@ -146,6 +164,21 @@ app.post('/api/persons',(request,response)=>{
 
 });
 
+app.put('/api/persons/:id', (request,response,next)=>{
+    const body= request.body
+
+    const person={
+        name:body.name,
+        number:body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id,person,{new:true})
+        .then(updatedPerson=>{
+            response.json(updatedPerson)
+        })
+        .catch(error=> next(error))
+})
+
 app.delete('/api/persons/:id', (request,response)=>{
     // const id = Number(request.params.id);
     // persons = persons.filter(person => person.id !== id);
@@ -156,6 +189,17 @@ app.delete('/api/persons/:id', (request,response)=>{
         .catch(error => next(error))
 });
 
+
+const errorHandler = (error, request, response, next)=>{
+    console.error(error.message);
+
+    if(error.name==='CastError'){
+        return response.status(400).send({error:'malformatter id'})
+    }
+    next(error)
+};
+
+app.use(errorHandler);
 
 // const PORT = process.env.PORT || 3001;
 //after adding PORT in .env:
